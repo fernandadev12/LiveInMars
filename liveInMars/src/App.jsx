00a1e66b541f781136a2941c035ef099889f8758
fernandadev12 <fernandaos12@hotmail.com>
2026-06-27 22:11:41 -0300

    tipografia da home

--- liveInMars/src/App.jsx
+++ liveInMars/src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 import { Aside } from "./components/Aside";
 import { SearchInput } from "./components/SearchInput";
 import { Main } from "./components/Main/main";
+import { Typography } from "./components/Typography";
 
 function App() {
   return (
@@ -12,6 +13,13 @@ function App() {
       <Aside />
       <Main>
         <SearchInput />
+        <div>
+          <Typography variant="h1">Curiosidades sobre Marte</Typography>
+          <Typography variant="body">
+            Nos cards trazemos muitas curiosidades sobre os mistérios do planeta
+            vermelho pelo olhar do curiosity.
+          </Typography>
+        </div>
       </Main>
     </Container>
   );
